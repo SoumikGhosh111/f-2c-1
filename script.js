8a315909149e83539c0954616f2775417088a366
SoumikGhosh111 <132730107+SoumikGhosh111@users.noreply.github.com>
2023-08-12 14:26:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+let arr = [
+    { id: 1, name: 'john', age: 18, profession: 'developer' },
+    { id: 2, name: 'jack', age: 20, profession: 'developer' },
+    { id: 3, name: 'karen', age: 19, profession: 'admin' },
+  ];
+  
+  // Don't worry about consoling these functions, they are already being called on the button clicks.
+  // Please don't change anything in the index.html file.
+  
+  function PrintDeveloper() {
+    //Write your code here , just console.log
+        console.log('Consoling Array Variable',arr.filter(devs => devs.profession === "developer")); 
+  }
+  
+  function addData() {
+    //Write your code here, just console.log
+    let newIntern = { 
+        id:4,
+        name: "susan", 
+        age: 20, 
+        profession: "intern"
+    }
+    arr.push(newIntern); 
+    console.log('Consoling Array Variable',arr); 
+
+  }
+  
+  function removeAdmin() {
+    //Write your code here, just console.log
+    let notAdmin = arr.filter(persons => persons.profession !== "admin"); 
+    console.log('Consoling Array Variable',notAdmin);  
+  }
+  
+  function concatenateArray() {
+    //Write your code here, just console.log
+    let arr2 = [
+        {
+            id: 5,
+            name: "Binod",
+            age: 23,
+            profession: "Associate"
+        }, 
+        { 
+            id: 6, 
+            name: "Vinod Khanna", 
+            age: 70, 
+            profession: "CEO"
+        }
+    ]
+    let afterConcat = arr.concat(arr2); 
+    console.log('Consoling Array Variable',afterConcat); 
+
+  }
+  
+  // Here is an example of how functions work,
+  // basically a function is a block of code which can directly access your 'arr' variable since arr is global.
+  // If I have a function called consoleArr(), that can directly access arr like this to console it.
+  
+  function consoleArr() {   
+    console.log('Consoling Array Variable', arr);
+    // Over here I can directly access the variable.
+  }
